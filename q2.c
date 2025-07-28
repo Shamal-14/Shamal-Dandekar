995d1de711346b8b4f22e274256420dc47c73f7c
Shamal-14 <shamaldandekar1010@gmail.com>
2025-07-28 17:10:32 +0530

    question 2
    answer

---
+++ q2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int year=2025;
+    if (year%4==0)
+    {
+        printf("The year is leap year");
+    }
+    else{
+        printf("The year is Not leap year");
+    }
+}
