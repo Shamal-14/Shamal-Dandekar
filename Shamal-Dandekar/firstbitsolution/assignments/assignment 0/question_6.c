8fa1981850020b060813af9841e98ddb3d433c97
Shamal-14 <shamaldandekar10@gmail.com>
2025-09-24 11:29:58 +0530

    Deleted lowercase folders to clean up repo structure

--- Shamal-Dandekar/firstbitsolution/assignments/assignment 0/question_6.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-        int num=20;
-        int square=(num*num);
-        int cube=(num*num*num);
-        printf("The square of given no.%d",square);
-        printf("The cube of given no.%d",cube);
-}
--- Shamal-Dandekar/firstbitsolution/assignments/assignment 0/question_7.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-    int min =90;
-    int hour=min/60;
-    int min_left=min%60;
-    printf("%d Hour %d Min",hour,min_left);
-
-}
