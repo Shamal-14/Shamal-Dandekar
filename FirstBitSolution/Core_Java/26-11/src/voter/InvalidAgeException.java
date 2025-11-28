bdac4dfaf65d1cec537cfc4bc3481b7d9a820878
Shamal-14 <shamaldandekar1010@gmail.com>
2025-11-28 18:40:52 +0530

    Add files via upload

---
+++ FirstBitSolution/Core_Java/26-11/src/voter/InvalidAgeException.java
@@ -0,0 +1,10 @@
+package voter;
+
+public class InvalidAgeException extends Exception {
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return " Invalid Age, You cant vote!";
+	}
+
+}
