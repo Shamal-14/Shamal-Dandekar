6b5520c0a7087b058d05549a046eb2f0c1f298e0
Shamal-14 <shamaldandekar1010@gmail.com>
2025-12-18 12:58:55 +0530

    Delete FirstBitSolution/Core_Java/HomeAutomationSystemProject directory

--- FirstBitSolution/Core_Java/HomeAutomationSystemProject/src/model/interfaces/Adjustable.java
+++
@@ -1,6 +0,0 @@
-package model.interfaces;
-
-public interface Adjustable {
-	void increaseLevel();
-	void decreaseLevel();
-}
--- FirstBitSolution/Core_Java/HomeAutomationSystemProject/src/model/interfaces/TemperatureControl.java
+++
@@ -1,6 +0,0 @@
-package model.interfaces;
-
-public interface TemperatureControl {
-	void setTemperature(int temp);
-
-}
